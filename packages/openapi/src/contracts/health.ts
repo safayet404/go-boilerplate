2f56fc9a1a217f8fa1ffb185b29f89360168d8ba
safayet404 <hossainsafayet187@gmail.com>
2026-03-12 00:35:15 +0600

    emails

---
+++ packages/openapi/src/contracts/health.ts
@@ -0,0 +1,20 @@
+import { ZHealthResponse } from '@boilerplate/zod';
+import { initContract } from "@ts-rest/core";
+import { z } from "zod";
+import { getSecurityMetadata } from "@/utils.js";
+
+
+
+const c = initContract();
+
+export const healthContract = c.router({
+  getHealth: {
+    summary: "Get health",
+    path: "/status",
+    method: "GET",
+    description: "Get health status",
+    responses: {
+      200: ZHealthResponse,
+    },
+  },
+});
